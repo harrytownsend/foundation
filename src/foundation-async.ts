a1b058675d3ad8684c6ff5e1984c2cd11367913d
Harry Townsend <harry.james.townsend@gmail.com>
2024-07-09 21:06:11 +0100

    Added the fInterval Class
    The fInterval class behaves similar to the setInterval function except that it can be controlled via an object. Additionally, the class will not allow more than one instance of the callback to be active at once.

--- src/foundation-async.ts
+++ src/foundation-async.ts
@@ -74,3 +74,68 @@ export class fMutex {
 		}
 	}
 }
+
+export class fInterval {
+	private _active: boolean = false;
+	private _locked: boolean = false;
+	private _lastTick: Date = new Date();
+
+	private _callback: Function;
+	private _interval: number;
+	private _spaced: boolean;
+
+	constructor(callback: Function, interval: number, spaced: boolean = true) {
+		this._callback = callback;
+		this._interval = interval;
+		this._spaced = spaced;
+
+		this.start();
+	}
+
+	public get active(): boolean { return this._active; }
+	public get callback(): Function { return this._callback; }
+	public get interval(): number { return this._interval; }
+	public get locked(): boolean { return this._locked; }
+	public get lastTick(): Date { return new Date(this._lastTick); }
+	public get spaced(): boolean { return this._spaced; }
+
+	public start(): void {
+		this._active = true;
+
+		if(!this._locked) {
+			this._tick();
+		}
+	}
+
+	public stop(): void {
+		this._active = false;
+	}
+
+	private async _tick(): Promise<void> {
+		while(this._active) {
+			this._lastTick = new Date();
+
+			this._locked = true;
+			await this._callback();
+			this._locked = false;
+
+			
+			if(this._active) {
+				if(this._spaced) {
+					await this._sleep(this._interval);
+				} else {
+					const millis = (new Date()).getTime() - this._lastTick.getTime();
+					if(millis < this._interval) {
+						await this._sleep(this._interval - millis);
+					}
+				}
+			}
+		}
+	}
+
+	private async _sleep(millis: number): Promise<void> {
+		await new Promise((resolve: Function, reject: Function) => {
+			setTimeout(() => { resolve(); }, millis);
+		});
+	}
+}
